fdd7241a55fbae34827d86d2fa6733ab980b3431
Nikita Nemtcev <nemtcevn@gmail.com>
2020-04-16 18:26:07 -0400

    Ready for Heroku

---
+++ client/build/precache-manifest.d4739632b1769158a2fc19ecf27ea34f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812c4639b1c9a1eb624f487673e51900",
+    "url": "/react-redux-crud-app/index.html"
+  },
+  {
+    "revision": "8bb4bdcc39bec727c5ad",
+    "url": "/react-redux-crud-app/static/js/2.f7835bbd.chunk.js"
+  },
+  {
+    "revision": "7ff3e8849394329239d343acaff0f565",
+    "url": "/react-redux-crud-app/static/js/2.f7835bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f3d96c30c9e40cc347",
+    "url": "/react-redux-crud-app/static/js/main.58e7a721.chunk.js"
+  },
+  {
+    "revision": "2bbad52d9c2ee869df70",
+    "url": "/react-redux-crud-app/static/js/runtime-main.3c72abd2.js"
+  }
+]);
